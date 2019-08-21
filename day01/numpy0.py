08fb4da2f1d64bfb90db430e94b08adb1151539c
zy723 <zy723@vip.qq.com>
2019-08-21 17:00:48 +0800

    create project

---
+++ day01/numpy0.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+@version: v1.0
+@contact: zy723@vip.qq.com
+@site: 
+@software: PyCharm
+@file: numpy0.py
+@time: 2019/8/21 16:49
+"""
+
+
+def func():
+    pass
+
+
+class Main(object):
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
+    pass
+ 

dfbef53189396a2b4f28d5601f1608b73866cf40
zy723 <zy723@vip.qq.com>
2019-09-09 16:55:17 +0800

    add deep study tensorflow

--- day03/tensorflow_test.py
+++ day03/tensorflow_test.py
@@ -12,7 +12,11 @@ import tensorflow as tf
 import os
 
 
-def tensorflow_test():
+def tensorflow_synchronize_read_data():
+    """
+    模拟同步读取数据
+    :return:
+    """
     # 首先定义列队
     Q = tf.FIFOQueue(3, tf.float32)
     # 存入数据
@@ -32,5 +36,49 @@ def tensorflow_test():
             print(sess.run(Q.dequeue()))
 
 
+def tensorflow_asynchronous_read_data():
+    """
+    模拟异步读取数据
+    :return:
+    """
+    # 定义一个列队
+    Q = tf.FIFOQueue(1000, tf.float32)
+
+    # 定义需要做的事情:循环自增
+    var = tf.Variable(0.0)
+
+    # 实现自增
+    data = tf.assign_add(var, tf.constant(1.0))
+    en_q = Q.enqueue(data)
+
+    # 3、定义队列管理器op, 指定多少个子线程，子线程该干什么事情
+    qr = tf.train.QueueRunner(Q, enqueue_ops=[en_q] * 2)
+
+    # 初始化变量op
+    init_op = tf.global_variables_initializer()
+
+    with tf.Session() as sess:
+        # 初始化变量
+        sess.run(init_op)
+
+        # 开启线程管理器
+        coord = tf.train.Coordinator()
+
+        # 开启真正的子线程
+        threads = qr.create_threads(sess, coord=coord, start=True)
+
+        # 主线程不断读取并训练数据
+        for i in range(300):
+            print(sess.run(Q.dequeue()))
+
+        # 回收子进程
+        coord.request_stop()
+
+        coord.join(threads)
+
+
 if __name__ == "__main__":
-    tensorflow_test()
+    # 同步
+    # tensorflow_synchronize_read_data()
+    # 异步
+    tensorflow_asynchronous_read_data()
